312cfb5eee6201df737cc6e48dbfe6acff776e08
Esteban <esteban.laisney@sts-sio-caen.info>
2018-03-27 09:31:31 +0200

    Ajout de routes step/story/tag

--- router.js
+++ router.js
@@ -21,6 +21,17 @@ Router.map(function() {
 
   this.route('story', function() {
     this.route('new', {path: 'new/:project_id'});
+    this.route('update', {path: 'update/:story_id'});
+    this.route('delete', {path: 'delete/:story_id'});
+  });
+  this.route('tag', function() {
+    this.route('update', {path: 'update/:tag_id'});
+    this.route('delete', {path: 'delete/:tag_id'});
+  });
+  this.route('step', function() {
+    this.route('new', {path: 'new/:step_id'});
+    this.route('update', {path: 'update/:step_id'});
+    this.route('delete', {path: 'delete/:step_id'});
   });
 });
 
