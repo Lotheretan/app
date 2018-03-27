9cc12abd495a146cb393715b565c9bd3df48040e
Erabillon <31841156+Lotheretan@users.noreply.github.com>
2018-03-27 12:09:01 +0200

    Gestion des Stories

--- routes/story/update.js
+++ routes/story/update.js
@@ -1,4 +1,27 @@
 import Route from '@ember/routing/route';
+import EmberObject, {set} from '@ember/object';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+  model(params){
+    return new RSVP.hash({
+      storyy: this.get('store').findRecord('story',params.story_id)
+    });
+  },
+  afterModel(model){
+    set(model,'data',EmberObject.create(JSON.parse(JSON.stringify(model.storyy))));
+  },
+  actions:{
+    save(storyy,data){
+      set(storyy,'code',data.code);
+      set(storyy,'description',data.description);
+
+      storyy.save().then(()=>{
+        this.transitionTo("project");
+      })
+    },
+    cancel(){
+      this.transitionTo("project");
+    }
+  }
 });
