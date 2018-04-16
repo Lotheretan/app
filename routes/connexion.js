42c1eabdc109dbfe80c583432a73fa8167671bff
ProdhommeR <romain.prodhomme@sts-sio-caen.info>
2018-04-16 09:04:53 +0200

    Connexion.js 2.0

--- routes/connexion.js
+++ routes/connexion.js
@@ -1,4 +1,26 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  session : Ember.inject.service(),
+  action : {
+    login(){
+      let {username, password}= this.getproperties('username', 'password');
+      this.get("session").login(username, password).then(()=>{
+        this.transitionToPreviousRoute()
+      }).catch((reason)=>{
+        console.log("Error : ",reason)
+      })
+
+    }
+  },
+  transitionToPreviousRoute(){
+    var previousTransition = this.get('previousTransition');
+    if (previousTransition){
+      this.set('previousTransition', null);
+      previousTransition.retry();
+    }else{
+      //Default back to homepage
+      this.transitionRoute('index');
+    }
+  }
 });
